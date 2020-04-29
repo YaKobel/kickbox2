1234cacddca66effe852563834c785b5165551bd
Yaroslav Kobel <yarokobel@gmail.com>
2020-04-29 23:37:37 +0300

    DEBUG = FALSE

--- kickbox/settings.py
+++ kickbox/settings.py
@@ -130,7 +130,7 @@ if os.getcwd() == '/app':
 
     #Allow all host headers
     ALLOWED_HOSTS = ['uakickbox.herokuapp.com']
-    DEBUG = False
+    DEBUG = True
 
     #Static asset configuration
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
