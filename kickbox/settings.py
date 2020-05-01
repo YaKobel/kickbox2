348b634dadfb631e5619f4859f1c43e0e9a436d0
Yaroslav Kobel <yarokobel@gmail.com>
2020-05-01 23:49:36 +0300

    debug

--- kickbox/settings.py
+++ kickbox/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm+e6n(#)drjn&%$rf$veny96uo1yh(t8*8o(dffbs1jbtl5_do'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
